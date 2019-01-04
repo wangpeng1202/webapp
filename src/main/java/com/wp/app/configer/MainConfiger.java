dc903a971478fe4cad5175b6f6b8385e0a3679f4
wangpeng1202 <wang.p122@foxmail.com>
2019-01-04 18:06:51 +0800

    编写登录页面

--- src/main/java/com/wp/app/configer/MainConfiger.java
+++ src/main/java/com/wp/app/configer/MainConfiger.java
@@ -1,6 +1,7 @@
 package com.wp.app.configer;
 
 import com.wp.app.componte.MyLocaleResolver;
+import com.wp.app.componte.MyWebMvcConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -11,5 +12,10 @@ public class MainConfiger {
     @Bean
     public LocaleResolver myLocaleResolver() {
         return new MyLocaleResolver();
+    }
+
+    @Bean
+    public MyWebMvcConfigurer myWebMvcConfigurer() {
+        return new MyWebMvcConfigurer();
     }
 }
--- src/main/java/com/wp/app/controller/LoginController.java
+++ src/main/java/com/wp/app/controller/LoginController.java
@@ -2,7 +2,13 @@ package com.wp.app.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -10,5 +16,17 @@ public class LoginController {
     @RequestMapping("/")
     public String index() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Map<String, Object> map, HttpSession session) {
+        if (!StringUtils.isEmpty(username) && password.equals("123")) {
+            session.setAttribute("loginUser", username);
+            return "redirect:dashboard";
+        } else {
+            return "login";
+        }
+
+
     }
 }
